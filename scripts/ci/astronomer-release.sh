010c43e59002e1feaaaba58488558648d1d82ce0
Kaxil Naik <kaxilnaik@gmail.com>
2020-03-18 18:56:13 +0000

    Build DEV packages on each commit if it is not a tag

--- scripts/ci/astronomer-release.sh
+++ scripts/ci/astronomer-release.sh
@@ -3,22 +3,39 @@
 # This file is Not licensed to ASF
 # SKIP LICENSE INSERTION
 
-set -euo pipefail
-MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 export PYTHON_VERSION=3.6
 
-# shellcheck source=scripts/ci/_utils.sh
-. "${MY_DIR}/_utils.sh"
+# shellcheck source=scripts/ci/_script_init.sh
+. "$( dirname "${BASH_SOURCE[0]}" )/_script_init.sh"
 
-cd "${AIRFLOW_SOURCES}"
+# If the build is not a tag/release, build a dev version
+if [[ -z ${TRAVIS_TAG:=} ]]; then
+    echo
+    AIRFLOW_VERSION=$(awk '/version/{print $NF}' airflow/version.py | tr -d \')
+    export AIRFLOW_VERSION
+    echo "Current Version is: ${AIRFLOW_VERSION}"
 
-basic_sanity_checks
+    if [[ ${AIRFLOW_VERSION} == *"dev"* ]]; then
+        echo "Building and releasing a DEV Package"
+        echo
+        git fetch --unshallow
 
-script_start
+        COMMITS_SINCE_LAST_TAG="$(git rev-list "$(git describe --abbrev=0 --tags)"..HEAD --count)"
+        sed -i -E "s/dev[0-9]+/dev${COMMITS_SINCE_LAST_TAG}/g" airflow/version.py
 
-python3 setup.py verify compile_assets sdist bdist_wheel
+        UPDATED_AIRFLOW_VERSION=$(awk '/version/{print $NF}' airflow/version.py | tr -d \')
+        export UPDATED_AIRFLOW_VERSION
+        echo "Updated Airflow Version: $UPDATED_AIRFLOW_VERSION"
+
+        python3 setup.py --quiet verify compile_assets sdist bdist_wheel
+    else
+        echo "Version does not contain 'dev' in airflow/version.py"
+        echo "Skipping build and release process"
+        echo
+        exit 1
+    fi
+elif [[ ${TRAVIS_TAG:=} == "${TRAVIS_BRANCH:=}" ]]; then
+    python3 setup.py --quiet verify compile_assets sdist bdist_wheel
+fi
 
 ls -altr dist/*
-
-script_end
