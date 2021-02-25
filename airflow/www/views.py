217aa1c1b4cec3609bb894cdbfd313053fb6c944
Max Taggart <max.taggart@gmail.com>
2021-02-25 12:16:55 -0700

    Unable to trigger backfill or manual jobs with Kubernetes executor. (#14160)
    closes: #13805
    (cherry picked from commit 2b5d4e3ff3c61ea6074caa300bbb8d16027408a6)

--- airflow/www/views.py
+++ airflow/www/views.py
@@ -1369,6 +1369,7 @@ class Airflow(AirflowBaseView):  # noqa: D101  pylint: disable=too-many-public-m
             )
             return redirect(origin)
 
+        executor.job_id = "manual"
         executor.start()
         executor.queue_task_instance(
             ti,
