9eb3773670b4b47f4ec531d3332a9c4c4917222c
ernest-kr <ernest@astronomer.io>
2021-03-26 18:25:50 -0700

    [astro] Publish AC 2.0.2.dev1+astro.1

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ INSTALL_PROVIDERS_FROM_SOURCES = 'INSTALL_PROVIDERS_FROM_SOURCES'
 
 logger = logging.getLogger(__name__)
 
-version = '2.0.2'
+version = '2.0.2.dev1+astro.1'
 
 my_dir = dirname(__file__)
 
