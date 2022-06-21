a65ad2b4831bc2b0f68bf61579d959579dab14cb
Ephraim Anierobi <splendidzigy24@gmail.com>
2022-06-21 14:01:16 +0100

    [astro]Fix log_id_template 2

--- airflow/utils/db.py
+++ airflow/utils/db.py
@@ -796,15 +796,23 @@ def seed_log_template(*, session: Session = NEW_SESSION) -> None:
         log.info('Log template table does not exist (added in 2.3.0); skipping log template seeding.')
         return
 
-    if session.query(LogTemplate.id).first():
-        return
-
     # The Astronomer chart overrode the default log_id_template to this
     log_id = "{dag_id}_{task_id}_{execution_date}_{try_number}"
     # While the log_filename_template was the default
     filename = "{{ ti.dag_id }}/{{ ti.task_id }}/{{ ts }}/{{ try_number }}.log"
 
-    session.add(LogTemplate(filename=filename, elasticsearch_id=log_id))
+    # We mistakenly seeded this in 2.3.0. We need to update it to the new format.
+    old_log_id = "{dag_id}_{task_id}_{run_id}_{try_number}"
+
+    updated = (
+        session.query(LogTemplate)
+        .filter(LogTemplate.elasticsearch_id == old_log_id)
+        .update({LogTemplate.elasticsearch_id: log_id})
+    )
+    if updated or session.query(LogTemplate.id).first():
+        return
+    else:
+        session.add(LogTemplate(filename=filename, elasticsearch_id=log_id))
 
 
 @provide_session
